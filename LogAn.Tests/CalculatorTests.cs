a8748c32fdff456904e90a508293adf934315dda
Ronye Rocha <ronye.rocha@tecnobank.com.br>
2022-09-09 15:50:04 -0300

    calculator

---
+++ LogAn.Tests/CalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace LogAn.Tests
+{
+    public class CalculatorTests
+    {
+        private Calculator _calculator;
+
+        public CalculatorTests()
+        {
+            _calculator = new Calculator();
+        }
+
+        [Fact]
+        public void Sum_NoAddCalls_DefautsToZero()
+        {
+            
+        }
+    }
+}
