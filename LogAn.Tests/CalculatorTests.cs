a8cc6d75314b7862ac5fd753f7c1f3b2494e0630
Ronye Rocha <ronye.rocha@tecnobank.com.br>
2022-09-11 00:32:18 -0300

    add two numbers tests

--- LogAn.Tests/CalculatorTests.cs
+++ LogAn.Tests/CalculatorTests.cs
@@ -14,7 +14,29 @@ namespace LogAn.Tests
         [Fact]
         public void Sum_NoAddCalls_DefautsToZero()
         {
+            //arrange && act
+            var result = _calculator.Sum();
+
+            //assert
+            Assert.Equal(0, result);
+        }
+
+        [Theory]
+        [InlineData(0, 1)]
+        [InlineData(1, 2)]
+        [InlineData(2, 4)]
+        public void Add_AddTwoNumbers_ReturnsExpectedResult(int firstNumber, int secondNumber)
+        {
+            //arrange && act
+
+            var result = firstNumber + secondNumber;
+
+            _calculator.Add(firstNumber);
+            _calculator.Add(secondNumber);
+
+            //assert
             
+            Assert.Equal(result, _calculator.Sum());   
         }
     }
 }
