3337de5bed08705c565ce56b6041684dba4854ce
Ronye Rocha <ronye.rocha@tecnobank.com.br>
2022-09-14 22:45:05 -0300

    refactor

---
+++ LogAn/FileExtensionManager.cs
@@ -0,0 +1,16 @@
+﻿namespace LogAn
+{
+    public class FileExtensionManager : IExtensionManager
+    {
+        public bool IsValid(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+                throw new ArgumentException("filename has to be provided");
+
+            if (!fileName.EndsWith(".SLF", StringComparison.CurrentCultureIgnoreCase))
+                return false;
+
+            return true;
+        }
+    }
+}
--- LogAn/LogAnalyzer.cs
+++ LogAn/LogAnalyzer.cs
@@ -6,17 +6,9 @@
 
         public bool IsValidLogFileName(string fileName)
         {
-            WasLastFileNameValid = false;
+            var manager = new FileExtensionManager();
 
-            if (string.IsNullOrEmpty(fileName))
-                throw new ArgumentException("filename has to be provided");
-
-            if (!fileName.EndsWith(".SLF", StringComparison.CurrentCultureIgnoreCase))
-                return false;
-
-            WasLastFileNameValid = true;
-
-            return true;
+            return manager.IsValid(fileName);
         }
     }
 }
