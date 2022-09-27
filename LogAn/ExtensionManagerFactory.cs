9c3c1ed514781738eca89cc5ac0504ef84ff2b07
Ronye Rocha <ronye.rocha@tecnobank.com.br>
2022-09-27 00:08:58 -0300

    commit

--- LogAn/ExtensionManagerFactory.cs
+++ LogAn/ExtensionManagerFactory.cs
@@ -12,6 +12,7 @@
                 return customManager;
             
             return new FileExtensionManager();
+            //oi, eu sou o goku e estou comitando isso num dia que não consegui estudar testes unitário. amanhã eu conseguiriei !!! <3
         }
     }
 }
