4566ed3fe9ee4303a5b485df643e38e3698532a5
Ronye do Lago <ronye.rocha@gmail.com>
2022-10-10 20:24:42 -0300

    simple parser, bro

---
+++ NumberParser/SimpleParser.cs
@@ -0,0 +1,17 @@
+﻿namespace NumberParser
+{
+    public class SimpleParser
+    {
+        public int ParseAndSum(string numbers)
+        {
+            if (numbers.Length == 0)
+                return 0;
+
+            if (!numbers.Contains(","))
+                return int.Parse(numbers);
+            else
+                throw new InvalidOperationException("I can only handle 0 or 1 numbers for now!");
+        }
+        
+    }
+}
