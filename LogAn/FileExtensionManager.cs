f6a739d08cbc73c1c8441765975d169b2e6e4ad8
Ronye do Lago <ronye.rocha@gmail.com>
2022-11-22 22:19:32 -0300

    ainda com erro nos testes

--- LogAn/FileExtensionManager.cs
+++ LogAn/FileExtensionManager.cs
@@ -7,7 +7,7 @@
             if (string.IsNullOrEmpty(fileName))
                 throw new ArgumentException("filename has to be provided");
 
-            if (!fileName.EndsWith(".SLF", StringComparison.CurrentCultureIgnoreCase) || Path.GetFileNameWithoutExtension(fileName).Length > 5)
+            if (!fileName.EndsWith(".SLF", StringComparison.CurrentCultureIgnoreCase))
                 return false;
 
             return true;
